09f780c4df1058953de0dc5244d0bd40c597c674
Никита Шалимов <NikitaShalimov@mail.ru>
2023-08-02 02:20:04 +0500

    add login, jwt, bcryptjs

--- client/src/utils/authHelper.js
+++ client/src/utils/authHelper.js
@@ -1,17 +1,26 @@
-//const user = { id: 123, username: 'exampleUser' };
+export const user = null;
+
+export const isAuth = (data) => {
+  if(typeof data === 'object') {
+    user = data;
+    console.log(user)
+  } else {
+    return 'нет пользователя';
+  }
+}
 
 // Функция для сохранения информации о залогиненном пользователе
-export const saveLoggedInUser = (user) => {
-	localStorage.setItem('loggedInUser', JSON.stringify(user));
-  };
+// export const saveLoggedInUser = (user) => {
+// 	localStorage.setItem('loggedInUser', JSON.stringify(user));
+//   };
   
-  // Функция для получения информации о залогиненном пользователе
-  export const getLoggedInUser = () => {
-	const loggedInUser = localStorage.getItem('loggedInUser');
-	return loggedInUser ? JSON.parse(loggedInUser) : null;
-  };
+//   // Функция для получения информации о залогиненном пользователе
+//   export const getLoggedInUser = () => {
+// 	const loggedInUser = localStorage.getItem('loggedInUser');
+// 	return loggedInUser ? JSON.parse(loggedInUser) : null;
+//   };
   
-  // Функция для выхода пользователя (очистка информации о статусе входа)
-  export const logoutUser = () => {
-	localStorage.removeItem('loggedInUser');
-  };
+//   // Функция для выхода пользователя (очистка информации о статусе входа)
+//   export const logoutUser = () => {
+// 	localStorage.removeItem('loggedInUser');
+//   };
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,46 +1,73 @@
 const Router = require('express');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require('express-validator')
 
 const router = new Router;
 const users = [];
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
 	try {
-		const {name, email, password} = req.body;
+		const { name, email, password } = req.body;
 
-		const user = users.find(item => item.name === name || item.email === email || item.password === password);
+		const user = users.find(item => item.name === name && item.email === email);
+		if (!user) {
+			return res.status(400).json({ message: 'User not found' });
+		}
 
-		if(!user) {
-			return res.status(400).json({message: 'The data is incorrect'});
-		} 
+		const isPassValid = bcrypt.compareSync(password, user.password)
+		if (!isPassValid) {
+			return res.status(400).json({ message: 'Invalid password' });
+		}
 
-		users.push(req.body);
-		console.log(users);
-		return res.json({message: 'User was created'})
+		const token = jwt.sign({ email: user.email }, config.get("secretKey"), { expiresIn: "1h" });
+		return res.json({
+			token,
+			user: {
+				name: user.name,
+				email: user.email
+			}
+		})
 
 	} catch (err) {
 		console.log(err);
-		res.send({message: 'Не получилось войти'})
+		res.send({ message: 'Server error' })
 	}
 })
 
-router.post('/registration', (req, res) => {
-	try {
-		const {name, email, password} = req.body;
 
-		const user = users.find(item => item.name === name || item.email === email);
+router.post('/registration',
+	[
+		check('email', 'Uncorrect email').isEmail(),
+		check('password', 'Password must be longer than 3 and shorter than 12').isLength({ min: 3, max: 12 }),
+	],
+	async (req, res) => {
+		try {
+			const errors = validationResult(req)
+			if (!errors.isEmpty()) {
+				return res.status(400).json({ message: "Uncorrect request", errors })
+			}
 
-		if(user) {
-			return res.status(400).json({message: 'User with name or email already exist'});
-		} 
+			const { name, email, password } = req.body;
 
-		users.push(req.body);
-		console.log(users);
-		return res.json({message: 'User was created'})
+			const user = users.find(item => item.name === name || item.email === email);
 
-	} catch (err) {
-		console.log(err);
-		res.send({message: 'Не получилось войти'})
-	}
-})
+			if (user) {
+				return res.status(400).json({ message: 'User with name or email already exist' });
+			}
+
+			const hashPassword = await bcrypt.hash(password, 8);
+			const newUser = { name, email, password: hashPassword };
+
+			users.push(newUser);
+			console.log(users);
+			return res.json({ message: 'User was created' })
+
+		} catch (err) {
+			console.log(err);
+			res.send({ message: 'Server error' })
+		}
+	})
 
 module.exports = router;
