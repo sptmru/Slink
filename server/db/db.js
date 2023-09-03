2bd3bd4784e2bc12e3b2bc79db70feadb762c183
Soslan Aldatov <soslanaldatov@gmail.com>
2023-09-03 16:24:22 +0400

    use env variables to connect to the database

--- server/db/db.js
+++ server/db/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-	user: 'postgres',
-	host: 'localhost',
-	database: 'Slink_db',
-	password: '1412',
-	port: 5432, // Порт PostgreSQL по умолчанию
+	user: process.env.DB_USER,
+	host: process.env.DB_HOST,
+	database: process.env.DB_NAME,
+	password: process.env.DB_PASSWORD,
+	port: Number(process.env.DB_PORT), // Порт PostgreSQL по умолчанию
 });
 
 function query(queryText, values) {
